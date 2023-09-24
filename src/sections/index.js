4a5abd10be35b5374dbe5e9611379cbc47e53776
GokulinCode <gokul@blockxsolutions.io>
2023-09-24 11:12:24 +0530

    complete

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CoustomerReviews from "./CoustomerReviews";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularProduct from "./PopularProduct";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Footer,
+  Hero,
+  PopularProduct,
+  SuperQuality,
+  CoustomerReviews,
+  Subscribe,
+  SpecialOffer,
+  Services,
+};
